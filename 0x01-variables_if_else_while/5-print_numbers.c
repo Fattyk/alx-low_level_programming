506768ef63903b2d8c79a266e834994e210d8dd6
root <user.name>
2022-04-18 09:28:27 -0700

    Add task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char k;
+
+	k = 48;
+	while (k <= 57)
+	{
+		putchar(k);
+		k++;
+	}
+	putchar(10);
+	return (0);
+}
