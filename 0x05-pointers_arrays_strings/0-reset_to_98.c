3042c06240ef9a547ca6c78dde857b4aeb40f2e2
root <user.name>
2022-04-20 15:50:19 -0700

    Write a function to update value with pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - use pointer to change variable value to 98
+ *
+ * @n: a pointer to an integer variable address
+ *
+ * Return: (0)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
