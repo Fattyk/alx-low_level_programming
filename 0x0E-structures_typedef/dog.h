7fc867917602a04c5a9016a9f74ba48e690e186a
root <user.name>
2022-05-11 15:55:10 -0700

    Add task 0: dog => how to define struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - Profile of a dog struct
+ * @name: string of input name
+ * @age: type int of input age
+ * @owner: type string of input owner
+ *
+ * This profile allow a dog information to be accessed and created through a
+ * single variable dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
