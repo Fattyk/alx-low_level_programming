ae3bd1a9b308ddee0817cc5ba94fee99534e9339
root <user.name>
2022-06-13 08:17:37 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,90 @@
 #include "main.h"
 
 /**
- * main - a program that multiplies two positive numbers
+ * print -  prints a string, followed by a new line
  * @argc: n arguments
  * @argv: args
  * Return: int
  */
-int main(int argc, char *argv[])
+void print(char *str)
 {
-	unsigned long mul;
-	int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-		exit(98); }
-	for (i = 1; i < argc; i++)
+	int i = 0;
+	while (str[i])
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-				exit(98); }
-		}
-
+		_putchar(str[i]);
+		i++;
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+
+}
+
+/**
+ * parseInt - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
+ */
+
+int parseInt(const char *s)
+{
+	int sign = 1;
+	unsigned long int result = 0, count, i;
+
+	for (count = 0; !(s[count] >= 48 && s[count] <= 57); count++)
+	{
+		if (s[count] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = count; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		result *= 10;
+		result += (s[i] - 48);
+	}
+
+	return (sign * result);
+}
+
+/**
+ * print_int - prints an integer.
+ * @n: int
+ * Return: 0
+ */
+
+void print_int(unsigned long int n)
+{
+
+	unsigned  long int divisor = 1, i, result;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		result = n / divisor;
+		_putchar('0' + result);
+	}
+
+}
+
+/**
+ * main - print the result of the multiplication, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		print("Error ");
+		exit(98);
+	}
+	print_int(parseInt(argv[1]) * parseInt(argv[2]));
+	_putchar('\n');
+
 	return (0);
 }
